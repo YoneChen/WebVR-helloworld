31a7360a4fa0a4c013be3b2655114d4cd3011dc3
陈昱宏 <chenyuhong508@pingan.com.cn>
2017-01-15 23:49:39 +0800

    init project
    	new file:   docs/index.css
    	new file:   docs/index.html
    	new file:   docs/index.js

---
+++ docs/index.js
@@ -0,0 +1,213 @@
+/**
+** author:YorkChan
+** date:2016-12-18
+**/
+var GazeTarget=0,Gazing=false,targetMesh;
+var vrMode = false;
+var TIME = 2000;
+var isMobile = function () {
+  	var check = false;
+  	(function (a) {
+    	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) {
+      	check = true;
+    	}
+  	})(navigator.userAgent || navigator.vendor || window.opera);
+  	return check;
+};
+function Index() {
+	this.init();
+}
+Index.prototype = {
+	init: function() {
+		var self = this;
+		//初始化场景
+		this.scene = new THREE.Scene();
+		//初始化相机
+		this.camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,0.1,4000);
+		this.camera.position.set( 0, 100, 0 );
+		this.createCrosshair(0.005);
+		this.scene.add(this.camera);
+		//初始化渲染器
+		this.renderer = new THREE.WebGLRenderer({ antialias: true } );
+		this.renderer.setSize(window.innerWidth,window.innerHeight);
+		this.renderer.setClearColor(0x519EcB);
+		this.renderer.shadowMapEnabled = true;
+		document.body.appendChild(this.renderer.domElement);
+		this.scene.fog	= new THREE.FogExp2( 0xd0e0f0, 0.0025 );
+		this.createLight();
+		this.createGround(10000,10000);
+
+		//帧率显示
+		this.stats = new Stats();
+		document.body.appendChild( this.stats.dom );
+		this.initGaze();
+
+		//创建立方体
+		this.createCube();
+		//事件绑定
+		this.bindEvent();
+		// 初始化VR视觉控件
+  		this.effect = new THREE.VREffect(this.renderer);
+  		this.vrControls = new THREE.VRControls(this.camera);
+		this.render();
+
+		if(isMobile()) document.querySelector('.btn-vr').style.display = 'block';
+	},
+	bindEvent: function() {
+		var self = this;
+		document.getElementById('vr').addEventListener('click',function () {
+		  	(!vrMode) ? vrMode = true : vrMode = false;
+		  	self.requestFullscreen();
+		  	self.onWindowResize();
+		});
+
+		document.addEventListener('fullscreenchange', function() {
+			self.onFullscreenChange();
+		});
+		document.addEventListener('mozfullscreenchange',  function() {
+			self.onFullscreenChange();
+		});
+
+		window.addEventListener( 'resize', function() {
+			self.onWindowResize();
+		}, false );
+		// this.domEvents   = new THREEx.DomEvents(self.camera, self.renderer.domElement);
+	},
+	initGaze: function() {
+
+		this.raycaster = new THREE.Raycaster();
+		this.center = new THREE.Vector2();
+		this.MESHLIST= [];
+	},
+
+	onWindowResize: function() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		if (vrMode) {
+			this.effect.setSize(window.innerWidth, window.innerHeight);
+		} else {
+			this.renderer.setSize(window.innerWidth, window.innerHeight);
+		}
+	},
+	requestFullscreen: function() {
+		var self = this;
+	  	var el = self.renderer.domElement;
+
+	  	if (!isMobile()) {
+	    	self.effect.setFullScreen(true);
+	    	return;
+	  	}
+
+	  	if (el.requestFullscreen) {
+	    	el.requestFullscreen();
+	  	} else if (el.mozRequestFullScreen) {
+	    	el.mozRequestFullScreen();
+	  	} else if (el.webkitRequestFullscreen) {
+	    	el.webkitRequestFullscreen();
+	  	}
+	},
+	onFullscreenChange: function(e) {
+  		var fsElement = document.fullscreenElement ||
+    	document.mozFullScreenElement ||
+    	document.webkitFullscreenElement;
+
+  		if (!fsElement) {
+    		vrMode = false;
+  		} else {
+    	// lock screen if mobile
+    	window.screen.orientation.lock('landscape');
+  		}
+	},
+	createCrosshair: function (size) {
+		var material = new THREE.LineBasicMaterial({color:0xffffff});
+		var geometry = new THREE.Geometry();
+		// crosshair
+		geometry.vertices.push(new THREE.Vector3(0, size, 0));
+		geometry.vertices.push(new THREE.Vector3(0, -size, 0));
+		geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+		geometry.vertices.push(new THREE.Vector3(size, 0, 0));    
+		geometry.vertices.push(new THREE.Vector3(-size, 0, 0));
+
+		var crosshair = new THREE.Line( geometry, material );
+		crosshair.position.set(0,0,-0.3);
+		this.camera.add(crosshair);
+	},
+	createCube: function () {
+		//创建立方体
+		var self = this;
+		var geometry = new THREE.CubeGeometry( 10,10,10);
+		var Cubematerial = new THREE.MeshLambertMaterial( { color: 0xef6500,needsUpdate: true,opacity:1,transparent:true} );
+		this.Cube = new THREE.Mesh( geometry, Cubematerial );
+		this.Cube.position.set(0,100,-50);
+		this.Cube.rotation.set(Math.PI/6,Math.PI/4,0);
+		this.scene.add(this.Cube);
+		this.MESHLIST.push(this.Cube);
+		this.Cube.gazeEvent = function() {
+			self.Cube.material.opacity = 0.5;
+		}
+		this.Cube.blurEvent = function() {
+			self.Cube.material.opacity = 1;
+		}
+	},
+	createLight: function() {
+        this.scene.add(new THREE.AmbientLight(0xFFFFFF));
+        var light = new THREE.DirectionalLight( 0xffffff, 0.3 );
+		light.position.set( 200, 450, 500 );
+		light.castShadow = true;
+		light.shadow.mapSize.width = 2048;
+		light.shadow.mapSize.height = 512;
+		light.shadow.camera.near = 100;
+		light.shadow.camera.far = 1200;
+		light.shadow.camera.left = -1000;
+		light.shadow.camera.right = 1000;
+		light.shadow.camera.top = 350;
+		light.shadow.camera.bottom = -350;
+		this.scene.add( light );
+	},
+
+	createGround: function(width,height) {
+		//  GROUND
+		// var gt = new THREE.TextureLoader().load( "./textures/road.jpg" );
+		this.gg = new THREE.PlaneBufferGeometry( width, height );
+		var gm = new THREE.MeshPhongMaterial( { color: 0x999999 } );
+		this.ground = new THREE.Mesh( this.gg, gm );
+		this.ground.rotation.x = - Math.PI / 2;
+		// this.ground.material.map.repeat.set( 100, 100 );
+		// this.ground.material.map.wrapS = THREE.RepeatWrapping;
+		// this.ground.material.map.wrapT = THREE.RepeatWrapping;
+		// note that because the ground does not cast a shadow, .castShadow is left false
+		this.ground.receiveShadow = true;
+		this.scene.add( this.ground );
+	},
+	gaze: function() { //
+	    this.raycaster.setFromCamera(this.center, this.camera);
+	    var intersects = this.raycaster.intersectObjects(this.MESHLIST);
+	    
+	    if (intersects.length > 0) { //凝视触发
+	    	if(Gazing) return; //只触发一次
+	    	Gazing = true;
+	      	targetMesh = intersects[0].object;
+	      	targetMesh.gazeEvent();
+	    } else{ 
+	    	if(Gazing) targetMesh.blurEvent();
+	    	Gazing = false;
+	    }
+	},
+	render: function() {
+		var self = this;
+		var render = function() {
+			if (vrMode) {
+				self.effect.render(self.scene, self.camera);
+			}  else {
+				self.renderer.render(self.scene,self.camera);
+			}
+			self.gaze();
+			self.vrControls.update();
+			requestAnimationFrame(render);
+			self.stats.update();
+		}
+		render();
+	}
+};
+
+new Index();
